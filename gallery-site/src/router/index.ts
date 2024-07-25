e7d7f4255ebe41231862046b020b9d1ba226d4bc
RumenMoysev <rumoysev@gmail.com>
2024-07-25 13:38:20 +0300

    add routeGuards

--- gallery-site/src/router/index.ts
+++ gallery-site/src/router/index.ts
@@ -1,3 +1,4 @@
+import * as userService from '@/services/userService'
 import AddPainting from '@/views/AddPainting.vue'
 import EditPainting from '@/views/EditPainting.vue'
 import Home from '@/views/Home.vue'
@@ -6,7 +7,7 @@ import MyProfile from '@/views/MyProfile.vue'
 import PaintingDetails from '@/views/PaintingDetails.vue'
 import PaintingsList from '@/views/PaintingsList.vue'
 import Register from '@/views/Register.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type NavigationGuardNext, type RouteLocationNormalized } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,12 +20,14 @@ const router = createRouter({
     {
       path: '/login',
       name: 'Login',
-      component: Login
+      component: Login,
+      beforeEnter: unAuthGuard
     },
     {
       path: '/register',
       name: 'Register',
-      component: Register
+      component: Register,
+      beforeEnter: unAuthGuard
     },
     {
       path: '/gallery',
@@ -42,21 +45,60 @@ const router = createRouter({
         {
           path: '/add-painting',
           name: 'AddPainting',
-          component: AddPainting
+          component: AddPainting,
+          beforeEnter: adminGuard
         },
         {
           path: ':id/edit-painting',
           name: 'EditPainting',
-          component: EditPainting
+          component: EditPainting,
+          beforeEnter: adminGuard
         }
       ]
     },
     {
       path: '/my-profile',
       name: 'MyProfile',
-      component: MyProfile
+      component: MyProfile,
+      beforeEnter: authGuard
     }
   ]
 })
+
+async function authGuard(to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) {
+  await waitForAuth()
+
+  if(!userService.isLoggedIn.value) {
+    next('/login')
+  } else {
+    next()
+  }
+}
+
+async function unAuthGuard(to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) {
+  await waitForAuth()
+
+  if(userService.isLoggedIn.value) {
+    next('/')
+  } else {
+    next()
+  }
+}
+
+async function adminGuard(to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) {
+  await waitForAuth()
+
+  if(userService.role.value === 'admin') {
+    next()
+  } else {
+    next('/')
+  }
+}
+
+async function waitForAuth() {
+  if (!userService.isLoggedIn.value && document.cookie.includes('userId=')) {
+    return await userService.getUser()
+  }
+}
 
 export default router
