ba43eaec2853c2dc675ea70db511bd74f282d98d
RumenMoysev <rumoysev@gmail.com>
2024-07-25 12:28:45 +0300

    Add edit painting and add painting functionality

--- gallery-site/src/services/paintingsService.ts
+++ gallery-site/src/services/paintingsService.ts
@@ -1,5 +1,6 @@
-import type { painting, paintingDetails } from '@/types/painting.js'
+import type { painting, paintingDetails, paintingFormData } from '@/types/painting.js'
 import internalFetch from '../lib/internalFetch.js'
+import type { paintingErrObject } from '@/types/errors.js'
 
 export async function getPaintings(): Promise<painting[]> {
     const response: Response = await internalFetch('GET', 'paintings')
@@ -19,10 +20,65 @@ export async function getPaintingDetails(paintingId:string): Promise<paintingDet
     return response.json()
 }
 
-export function likePainting(paintingId: string) {
+export async function addPainting(paintingData: paintingFormData): Promise<Response> {
+    const createdAtTime = new Date().toLocaleString('en-GB', { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', timeZoneName: 'shortOffset' })
+    return await internalFetch('POST', 'paintings', {...paintingData, createdAtTime})
+}
+
+export async function editPainting(paintingData: paintingFormData, paintingId: string): Promise<Response> {
+    const updatedAtTime = new Date().toLocaleString('en-GB', { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', timeZoneName: 'shortOffset' })
+    return await internalFetch('PUT', `paintings/${paintingId}`, { ...paintingData, updatedAtTime })
+}
+
+export function likePainting(paintingId: string): Promise<Response> {
     return internalFetch('POST', `paintings/${paintingId}/like`)
 }
 
-export function deletePainting(paintingId: string) {
+export function deletePainting(paintingId: string): Promise<Response> {
     return internalFetch('DELETE', `paintings/${paintingId}`)
+}
+
+export function validate(paintingData: paintingFormData): paintingErrObject {
+    const titleLength: number = 4
+    const summaryLength: number = 10
+    const descriptionLength: number = 20
+    const imageUrlLength: number = 10
+    const minPrice: number = 1
+
+    const errObj: paintingErrObject = {}
+
+    if (!paintingData.title) {
+        errObj.title = 'Title is required!'
+    } else if (paintingData.title.length < titleLength) {
+        errObj.title = `Title should be at least ${titleLength} characters`
+    }
+
+    if( !paintingData.summary) {
+        errObj.summary = 'Summary is required!'
+    } else if (paintingData.summary.length < summaryLength) {
+        errObj.summary = `Summary should be at least ${summaryLength} characters`
+    }
+
+    if (!paintingData.description) {
+        errObj.description = 'Description is required!'
+    } else if (paintingData.description.length < descriptionLength) {
+        errObj.description = `Description should be at least ${descriptionLength} characters`
+    }
+
+    if (!paintingData.imageUrl) {
+        errObj.imageUrl = 'ImageUrl is required!'
+    } else if (paintingData.imageUrl.length < imageUrlLength) {
+        errObj.imageUrl = `ImageUrl should be at least ${imageUrlLength}`
+    } 
+    else if (!paintingData.imageUrl.startsWith('http://') && !paintingData.imageUrl.startsWith('https://')) {
+        errObj.imageUrl = 'Please provie a valid image URL'
+    }
+
+    if(!paintingData.price) {
+        errObj.price = 'Price is required!'
+    } else if (paintingData.price < minPrice) {
+        errObj.price = `Painting price cannot be less than ${minPrice}€ !`
+    }
+
+    return errObj
 }
